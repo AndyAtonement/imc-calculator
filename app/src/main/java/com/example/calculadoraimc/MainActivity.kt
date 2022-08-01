442d5cac9ef10f160b5293381d82426a7d45004a
AndyAtonement <andyatonement@hotmail.com>
2022-08-01 17:34:03 -0300

    Changes in the MainActivity
    Refoctoring the backend.

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -12,9 +12,6 @@ import com.example.calculadoraimc.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var uriUrl: Uri
-    lateinit var lauchBrowser: Intent
-    lateinit var answer: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,47 +28,33 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     fun calculate() {
-        val btnCalculate = binding.btnCalculate
-        val height = binding.etHeight
-        val weight = binding.etWeight
-        val result = binding.tvResult
-
-        btnCalculate.setOnClickListener{
+        binding.btnCalculate.setOnClickListener{
             var heightValue = 0.0
             var weightValue = 0.0
 
-            if(height.text.toString().isNotEmpty()) {
-                heightValue = height.text.toString().toDouble()
-            }
-            if(weight.text.toString().isNotEmpty()){
-                weightValue = weight.text.toString().toDouble()
-            }
+            if(binding.etHeight.text.toString().isNotEmpty()) heightValue = binding.etHeight.text.toString().toDouble()
+
+            if(binding.etWeight.text.toString().isNotEmpty()) weightValue = binding.etWeight.text.toString().toDouble()
+
             if(weightValue > 0.0 && heightValue > 0.0) {
                 val imcValue = String.format(" %.2f", weightValue / (heightValue * heightValue))
                 val imcDouble = imcValue.toDouble()
-                result.text = "Seu IMC é ${String.format("%.2f", imcDouble)}, você está " + "${imcResults(weightValue / (heightValue * heightValue))}"
+                binding.tvResult.text = "Seu IMC é ${String.format("%.2f", imcDouble)}, você está " + "${imcResults(weightValue / (heightValue * heightValue))}"
 
             }
             else {
                 Toast.makeText(
-                    this, "Por favor, insira altura e peso maiores que 0", Toast.LENGTH_LONG).show()
+                    this, "Por favor, insira altura e peso maiores que 0.", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     fun imcResults(imc: Double) :String{
-
-        if(imc < 18.5){
-            answer="Abaixo do peso"
-        } else if(imc > 18.5 && imc < 24.9) {
-            answer="Normal"
-        } else if(imc > 24.9 && imc < 30) {
-            answer="Acima do peso."
-        } else {
-            answer="Obeso"
+        when(imc) {
+            in 0.0..18.5 -> return "abaixo do peso."
+            in 18.5..24.9 -> return "normal."
+            in 25.0..29.9 -> return "acima do peso."
+            else -> return "obeso."
         }
-        return answer
     }
-
-
 }
