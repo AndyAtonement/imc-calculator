ed61ec47a833359e99e6dfbe02cbba489ce3b116
AndyAtonement <andyatonement@hotmail.com>
2022-08-02 11:08:48 -0300

    updates

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.calculadoraimc
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import com.example.calculadoraimc.databinding.ActivityMainBinding
 
